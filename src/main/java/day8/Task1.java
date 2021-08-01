cf94502e7ff0326251e80ed976878483327d9d9e
Ilya Vysokolyan <rd2d@list.ru>
2021-08-02 00:13:59 +0300

    complete day 8 tasks

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -1,7 +1,29 @@
 package day8;
 
+import java.util.Locale;
+
 public class Task1 {
     public static void main(String[] args) {
+        String str =" ";
+        long start = System.currentTimeMillis();
+        for(int i=0;i<=20000;i++){System.out.print(" "+i);}
+        long finish = System.currentTimeMillis()-start;
+
+
+
+        long start2 = System.currentTimeMillis();
+        StringBuilder stringBuilder = new StringBuilder(" ");
+        for(int i =0;i<=20000;i++){ System.out.print(stringBuilder.toString()+i);}
+        long finish2  = System.currentTimeMillis()-start2;
+
+
+        System.out.println();
+        System.out.println("Str = "+finish+" мс");
+        System.out.println("StringBuilder = "+finish2+" мс");
+
+
+
+
 
     }
 }
