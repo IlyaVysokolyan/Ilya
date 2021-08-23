b4a783d4dca7e5036b32461a343c8f9ca89e2132
vysokolyanilya <Steep4488>
2021-08-24 01:07:30 +0300

    complete day 17 tasks

--- src/main/java/day16/Task2.java
+++ src/main/java/day16/Task2.java
@@ -1,12 +1,8 @@
 package day16;
 
-import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -15,28 +11,36 @@ public class Task2 {
         PrintWriter file1 = new PrintWriter("file1.txt");
         PrintWriter file2 = new PrintWriter("file2.txt");
         Random random = new Random();
-        List<Integer> array = new ArrayList<>();
-//       array.add(3);array.add(8);array.add(7);array.add(5);array.add(28);array.add(73);array.add(4);array.add(1);array.add(5);
-//       array.add(32);array.add(89);array.add(12); // ДЛЯ ПРОВЕРКИ
-        //ЗАПИСТЬ В ФАЙЛ1
+
+        int count = 0;
+        int sum = 0;
+        //Запись в файл
         for (int i = 0; i < 1000; i++) {
-            int rnd = random.nextInt(1000 + 1);
-            array.add(rnd);
+            int rnd = random.nextInt(1000+ 1);
             file1.write(rnd + " ");
         }
-        int counterNumbersFile2 = 0; //ПРОВЕРКА КОЛ-ВА ЧИСЕЛ В ФАЙЛЕ2
-        //ЗАПИСЬ В ФАЙЛ2
-        for (int i = 0; i < array.size() - 19; i += 20) {
-            int sum = 0;
-            for (int j = i; j < i + 20; j++) {
-                sum += array.get(j);
-            }
-            file2.write((double) sum / 20 + " ");
-            counterNumbersFile2++;
-        }
-        System.out.println(" КОЛ-ВО ЧИСЕЛ В ФАЙЛЕ2 ----->" + counterNumbersFile2);
         file1.close();
+
+
+        File fullFile1 = new File("file1.txt");
+        Scanner scanner1 = new Scanner(fullFile1);
+        String line  = scanner1.nextLine();
+        String [] numbers = line.split(" ");
+
+        for(String num:numbers){
+            sum+=Integer.parseInt(num);
+            count++;
+            if (count == 20) {
+                file2.write((double)sum / count + " ");
+                sum = 0;
+                count = 0;
+            }
+        }
+        scanner1.close();
         file2.close();
+        scanner1.close();
+
+
         File file = new File("file2.txt");
         printResult(file);
     }
--- src/main/java/day17/Task1.java
+++ src/main/java/day17/Task1.java
@@ -2,6 +2,11 @@ package day17;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        ChessPiece[] mass = new ChessPiece[]{
+                ChessPiece.PAWN_WHITE, ChessPiece.PAWN_WHITE, ChessPiece.PAWN_WHITE, ChessPiece.PAWN_WHITE,
+                ChessPiece.ROOK_BLACK, ChessPiece.ROOK_BLACK, ChessPiece.ROOK_BLACK, ChessPiece.ROOK_BLACK};
+        for (ChessPiece pawn : mass) {
+            System.out.print(pawn.getFigure() + " ");
+        }
     }
 }
